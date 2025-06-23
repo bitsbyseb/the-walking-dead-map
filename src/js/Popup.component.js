3df98d4285f87c423876b920d54c10b8a1f69a90
Johan Puentes <puentespulidojohansebastian@gmail.com>
2025-06-23 16:42:43 -0500

    first commit

---
+++ src/js/Popup.component.js
@@ -0,0 +1,64 @@
+class Popup extends HTMLElement {
+    static observedAttributes = ["nombre", "estado", "coordenadas", "temporadas", "descripcion"];
+
+    constructor() {
+        super();
+        this.nombre = this.getAttribute('nombre');
+        this.estado = this.getAttribute('estado');
+        this.coordenadas = this.getAttribute('coordenadas');
+        this.temporadas = this.getAttribute('temporadas');
+        this.descripcion = this.getAttribute('descripcion');
+    }
+
+    get template() {
+        const template = document.createElement('template');
+        const seasons = this.temporadas.split('-');
+        const seasonsList = seasons.map(x => `<li>${x}</li>`);
+        console.log(seasonsList);
+        template.innerHTML = `
+            ${this.styles}
+            <h2>${this.nombre}</h2>
+            <img src="/src/public/places/${this.nombre}.webp" alt="${this.nombre}">
+            <p>${this.descripcion}</p>
+            <h3>Temporadas</h3>
+            <ul>
+                ${seasonsList.join("\n")}
+            </ul>
+        `;
+        return template;
+    }
+
+    get styles() {
+        return `<style>
+            img {
+                width:100%;
+                height: 10rem;
+            }
+        </style>`;
+    }
+
+    render() {
+        this.appendChild(this.template.content.cloneNode(true));
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    disconnectedCallback() {
+        this.remove();
+    }
+
+
+    attributeChangedCallback(current,oldValue,newValue) {
+        if (oldValue !== newValue) {
+            this[current] = newValue;
+        }
+    }
+
+    toString() {
+        return this.innerHTML;
+    }
+}
+
+window.customElements.define('popup-element', Popup);
---
+++ src/js/main.js
@@ -0,0 +1,58 @@
+'use strict';
+import "./Popup.component.js";
+
+/**
+ * @typedef placeInfo
+ * @property {string} nombre
+ * @property {string} estado
+ * @property {number[]} coordenadas
+ * @property {string} temporadas
+ * @property {string} descripcion
+ * @returns {Promise<placeInfo[]>}
+ */
+async function getPlacesData() {
+  const res = await fetch('../../data.json');
+  const data = await res.json();
+  console.log(data);
+  return data;
+}
+
+const map = L.map('map').setView([34.0, -84.0], 6);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  maxZoom: 18,
+}).addTo(map);
+
+
+const places = await getPlacesData();
+const coordenadas = places.map(x => x.coordenadas);
+for (const place of places) {
+  const circle = L.circle(place.coordenadas, {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 2 * 1000
+  });
+  circle.addTo(map);
+
+  const temporalParentNode = document.createElement('div');
+
+  const component = document.createElement('popup-element');
+  component.setAttribute('nombre', place.nombre);
+  component.setAttribute('estado', place.estado);
+  component.setAttribute('temporadas', place.temporadas);
+  component.setAttribute('descripcion', place.descripcion);
+  component.setAttribute('coordenadas', place.coordenadas);
+
+  temporalParentNode.appendChild(component);
+
+  circle.bindPopup(`${temporalParentNode.innerHTML}`);
+}
+
+const path = L.polyline(coordenadas, {
+  color: 'blue',      // color del trazo
+  weight: 4,         // grosor
+  opacity: 0.8,      // transparencia
+  dashArray: '5, 10' // linea discontinua
+}).addTo(map);
